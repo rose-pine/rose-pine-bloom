0a9fc902ee1b478e73c1bc39a3ec8a08a9103adb
Patrick Haun <patrick@bomgar.email>
2025-08-21 20:46:04 +0200

    remove cobra-cli description

--- cmd/root.go
+++ cmd/root.go
@@ -10,14 +10,7 @@ import (
 var rootCmd = &cobra.Command{
 	Use:   "bloom",
 	Short: "The Rosé Pine theme generator",
-	Long: `The Rosé Pine theme generator.
-
-A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Long: `The Rosé Pine theme generator.`,
 	// Uncomment the following line if your bare application
 	// has an action associated with it:
 	// Run: func(cmd *cobra.Command, args []string) { },
