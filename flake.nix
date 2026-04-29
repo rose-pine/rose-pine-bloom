10737167c207a594eccc04d22135c4c6809d5578
Julia <info@juliamertz.dev>
2026-04-30 01:24:03 +0200

    feat: nix flake (#88)

---
+++ flake.nix
@@ -0,0 +1,69 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    systems.url = "github:nix-systems/default";
+    nix-filter.url = "github:numtide/nix-filter";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    systems,
+    nix-filter,
+  }: let
+    eachSystem = nixpkgs.lib.genAttrs (import systems);
+    filter = nix-filter.lib;
+  in {
+    packages = eachSystem (
+      system: let
+        pkgs = import nixpkgs {inherit system;};
+      in {
+        default = pkgs.buildGoModule {
+          pname = "rose-pine-bloom";
+          version = "0.0.0";
+
+          src = filter {
+            root = ./.;
+            include = [
+              ./go.mod
+              ./go.sum
+              ./main.go
+              ./builder
+              ./cmd
+              ./color
+              ./config
+              ./template
+              ./version
+              ./docs
+            ];
+          };
+
+          vendorHash = "sha256-NW40XkNXAYJ6Y/lPNcKDTNSGljlA9kl4FpQSVCMrfw0=";
+
+          meta.mainProgram = "rose-pine-bloom";
+        };
+        rose-pine-bloom = self.packages.${system}.default;
+      }
+    );
+
+    devShells = eachSystem (
+      system: let
+        pkgs = import nixpkgs {inherit system;};
+      in {
+        default = pkgs.mkShell {
+          packages = [
+            pkgs.go
+            pkgs.gopls
+          ];
+        };
+        bloom = pkgs.mkShell {
+          packages = [self.packages.${system}.rose-pine-bloom];
+        };
+      }
+    );
+
+    overlays.default = final: prev: {
+      rose-pine-bloom = self.packages.${final.stdenv.hostPlatform.system}.rose-pine-bloom;
+    };
+  };
+}
